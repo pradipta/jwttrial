99777c9dfe59c79fd6b76fb619ce24e094604604
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-01-31 09:57:45 +0530

    jwt authorization

--- src/main/java/com/pradipta/jwttrial/security/SecurityConfigurer.java
+++ src/main/java/com/pradipta/jwttrial/security/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package com.pradipta.jwttrial.security;
 
+import com.pradipta.jwttrial.filter.JwtFilter;
 import com.pradipta.jwttrial.user.MyUserDetailsService;
 import com.pradipta.jwttrial.util.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +8,24 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    JwtFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -32,7 +39,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .antMatchers("/authenticate")
                 //.hasAnyRole("ADMIN", "USER")
                 .permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
